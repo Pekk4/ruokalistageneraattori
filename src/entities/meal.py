8a947cf24caf5b7e3771f9d5a556e3c65da93061
Pekka <75335748+Pekk4@users.noreply.github.com>
2022-04-15 20:33:12 +0300

    Added database id as attribute to entities.Meal

--- src/entities/meal.py
+++ src/entities/meal.py
@@ -1,6 +1,7 @@
 class Meal:
-    def __init__(self, name):
+    def __init__(self, name, db_id=None):
         self.name = name
+        self.id = db_id
 
     def __str__(self) -> str:
         return self.name
