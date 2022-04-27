22242da54396483f666d291d5c1b474360c71651
Pekka <75335748+Pekk4@users.noreply.github.com>
2022-04-27 22:04:46 +0300

    Updated schema.sql, still as a draft

--- src/schema.sql
+++ src/schema.sql
@@ -2,4 +2,13 @@ CREATE TABLE meals (id SERIAL PRIMARY KEY, name TEXT);
 CREATE TABLE users (id SERIAL PRIMARY KEY, username UNIQUE TEXT, password TEXT);
 --CREATE TABLE menus (id SERIAL PRIMARY KEY, meal_ids INTEGER REFERENCES meals, user_id INTEGER REFERENCES users, date DATE);
 --CREATE TABLE menus (id SERIAL PRIMARY KEY, meal_ids INTEGER[], user_id INTEGER, date DATETIME);
-CREATE TABLE menus (id SERIAL PRIMARY KEY, meal_ids INTEGER[], user_id INTEGER, date TIMESTAMP);
+--CREATE TABLE menus (id SERIAL PRIMARY KEY, meal_ids INTEGER[], user_id INTEGER, TIMESTAMP TIMESTAMP);
+CREATE TABLE menus (id SERIAL PRIMARY KEY, user_id INTEGER, timestamp TIMESTAMP, week_number, INTEGER);
+create table menu_meals (id SERIAL PRIMARY KEY, menu_id INTEGER REFERENCES menus, meal_id INTEGER REFERENCES meals);
+
+CREATE TABLE menus (id SERIAL PRIMARY KEY, user_id INTEGER, timestamp TIMESTAMP);
+CREATE UNIQUE INDEX test ON menus (user_id, DATE_PART('week', timestamp), DATE_PART('year', timestamp));
+
+--INSERT INTO menus (user_id, timestamp) VALUES (1, now()) ON CONFLICT (user_id, DATE_PART('week', timestamp), DATE_PART('year', timestamp)) DO UPDATE SET timestamp = '2022-04-19 16:30:30.416533';
+
+--menu_meals:ille indexi poistoa varten?
