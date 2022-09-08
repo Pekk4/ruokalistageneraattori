979ff44e48167c64f593fbddc2338af6f1dc0421
Pekka <75335748+Pekk4@users.noreply.github.com>
2022-09-08 13:35:23 +0300

    Updated config.py Added logging config path and log files directory creation

--- src/config.py
+++ src/config.py
@@ -1,11 +1,16 @@
-from os import getenv, path
 from dotenv import load_dotenv
+from os import getenv, path, mkdir
 
 dirname = path.dirname(__file__)
 envfile_path = path.join(dirname, "..", ".env")
+logfiles_path = path.join(dirname, "..", "logs")
 
 if path.exists(envfile_path):
     load_dotenv(envfile_path)
 
+if not path.exists(logfiles_path):
+    mkdir(logfiles_path)
+
 DATABASE_URL = getenv("DATABASE_URL")
 SECRET_KEY = getenv("SECRET_KEY")
+LOGGER_CONFIG_FILE = getenv("LOGGER_CONFIG_FILE")
