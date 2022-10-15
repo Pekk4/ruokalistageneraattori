0c97cf9d8c07f1339d36b488058ac1513550e5ae
Pekka <75335748+Pekk4@users.noreply.github.com>
2022-10-15 22:36:51 +0300

    Swapped keys/values in DAYS to utilize the dict better

--- src/utilities.py
+++ src/utilities.py
@@ -2,15 +2,14 @@ from datetime import date
 
 
 DAYS = {
-    "Maanantai": 0,
-    "Tiistai": 1,
-    "Keskiviikko": 2,
-    "Torstai": 3,
-    "Perjantai": 4,
-    "Lauantai": 5,
-    "Sunnuntai": 6
+    0: "Maanantai",
+    1: "Tiistai",
+    2: "Keskiviikko",
+    3: "Torstai",
+    4: "Perjantai",
+    5: "Lauantai",
+    6: "Sunnuntai",
 }
-
 
 def validate_week_number(week_number: int):
     weeks_this_year = date(date.today().year, 12, 28).isocalendar()[1]
@@ -27,3 +26,4 @@ def validate_year(year: int):
         return True
 
     raise ValueError("invalid year")
+
