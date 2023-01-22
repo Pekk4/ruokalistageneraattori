d8e2b67a4a78e99aa590dcb41f953d1a65c77bae
Peksu <75335748+Pekk4@users.noreply.github.com>
2023-01-22 14:41:49 +0200

    Added TailwindCSS config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
       height: {
         //'62': '15.5rem',
         '62': '80rem',
+        '100': '650px',
       },
       borderWidth: {
         DEFAULT: '1px',
@@ -15,7 +16,15 @@ module.exports = {
       fontWeight: {
         mediumbold: 600,
       },
+      textShadow: {
+        'sm-black': '1px 1px 2px rgb(0 0 0);',
+        'sm-emerald': '1px 1px 2px rgb(22 163 74);',
+        '2md': '2px 2px 3px rgb(255 255 255 / 95%);',
+        '3md': '3px 3px 4px rgb(255 255 255);',
+        '3xl': '0px 1px 2px rgba(0, 0, 0, 0.5), 1px 2px 4px rgb(0, 0, 0)',
+        '4xl': '1px 2px 3px rgba(255, 255, 255, 0.5), 2px 4px 6px rgb(255, 255, 255)',
+      },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-textshadow')],
 }
