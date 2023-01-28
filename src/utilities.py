85bb3468e0c35ffc53faa159feb769c8ab76e8b1
Peksu <75335748+Pekk4@users.noreply.github.com>
2023-01-28 21:58:10 +0200

    Gathered user informing messages to a centralized list in utilities
    Centralized informing messages instead of having a lot of redundant
    message strings in service classes, which are using them in error
    handling situations for informing user about errors or problems.

--- src/utilities.py
+++ src/utilities.py
@@ -28,6 +28,31 @@ QTY_UNITS = [
     "pss"
 ]
 
+MESSAGES = {
+    "no_meals":
+        "Sinulla ei ole vielä yhtään ruokalajia. \N{slightly frowning face}<br>" \
+        "<br>Lisää ensin ruokalajeja kirjastoosi generoidaksesi niistä ruokalistan.",
+    "common_error":
+        "Jotain meni valitettavasti pieleen. \N{crying face}<br>" \
+        "Yritä hetken päästä uudestaan, tai ota yhteyttä ylläpitoon.",
+    "input_error":
+        "Tyhjät merkkijonot niminä eivät ole sallittuja. \N{angry face}",
+    "meal_exists":
+        "Samanniminen ruokalaji löytyy jo kirjastostasi, " \
+        "haluatko päivittää sen antamillasi tiedoilla?",
+    "not_enough":
+        "Kirjastossasi ei ole tarpeeksi ruokalajeja ruokalistan luomiseksi." \
+        "Lisää ensin ruokalajeja kirjastoosi.",
+    "no_user":
+        "Käyttäjätunnusta ei löydy, tarkista kirjoitusasu. \N{crying face}",
+    "wrong_pass":
+        "Väärä salasana, yritä uudestaan. \N{face with monocle}",
+    "invalid_pass":
+        "Salasana ei ole kelvollinen, noudata annettuja ohjeita! \N{angry face}",
+    "invalid_uname":
+        "Käyttäjänimi ei ole kelvollinen, noudata annettuja ohjeita! \N{angry face}",
+}
+
 def validate_week_number(week_number: int):
     weeks_this_year = date(date.today().year, 12, 28).isocalendar()[1]
 
